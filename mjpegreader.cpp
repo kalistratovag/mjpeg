2d9d09890e79d335eb140b98d099919033b9a349
kalistratovag <alexander.kalistratov@itseez.com>
2015-03-30 16:13:50 +0300

    Updating functionality of mjpegreader

--- mjpegreader.cpp
+++ mjpegreader.cpp
@@ -11,21 +11,128 @@ namespace mjpeg
 {
 
 #define fourCC(a,b,c,d)   ((int)((uchar(d)<<24) | (uchar(c)<<16) | (uchar(b)<<8) | uchar(a)))
+#define fourCC_str(a)     ((int)((uchar(a[3])<<24) | (uchar(a[2])<<16) | (uchar(a[1])<<8) | uchar(a[0])))
+#define RIFF_CC           fourCC_str("RIFF")
+#define LIST_CC           fourCC_str("LIST")
+#define HDRL_CC           fourCC_str("hdrl")
+#define AVIH_CC           fourCC_str("avih")
+#define STRL_CC           fourCC_str("strl")
+#define STRH_CC           fourCC_str("strh")
+#define VIDS_CC           fourCC_str("vids")
+#define MJPG_CC           fourCC_str("MJPG")
+#define STRF_CC           fourCC_str("strf")
+#define AVI_CC            fourCC_str("AVI ")
 
-struct avi_header
+typedef uint32_t DWORD;
+typedef uint16_t WORD;
+typedef int32_t  LONG;
+
+
+#pragma pack(push, 1)
+struct AviMainHeader
 {
-    uint32_t dwMicroSecPerFrame;    //  The period between video frames
-    uint32_t dwMaxBytesPerSec;      //  Maximum data rate of the file
-    uint32_t dwReserved1;           // 0
-    uint32_t dwFlags;               //  0x10 AVIF_HASINDEX: The AVI file has an idx1 chunk containing an index at the end of the file.
-    uint32_t dwTotalFrames;         // Field of the main header specifies the total number of frames of data in file.
-    uint32_t dwInitialFrames;       // Is used for interleaved files
-    uint32_t dwStreams;             // Specifies the number of streams in the file.
-    uint32_t dwSuggestedBufferSize; // Field specifies the suggested buffer size forreading the file
-    uint32_t dwWidth;               // Fields specify the width of the AVIfile in pixels.
-    uint32_t dwHeight;              // Fields specify the height of the AVIfile in pixels.
-    uint32_t dwReserved[4];         // 0, 0, 0, 0
+    DWORD dwMicroSecPerFrame;    //  The period between video frames
+    DWORD dwMaxBytesPerSec;      //  Maximum data rate of the file
+    DWORD dwReserved1;           // 0
+    DWORD dwFlags;               //  0x10 AVIF_HASINDEX: The AVI file has an idx1 chunk containing an index at the end of the file.
+    DWORD dwTotalFrames;         // Field of the main header specifies the total number of frames of data in file.
+    DWORD dwInitialFrames;       // Is used for interleaved files
+    DWORD dwStreams;             // Specifies the number of streams in the file.
+    DWORD dwSuggestedBufferSize; // Field specifies the suggested buffer size forreading the file
+    DWORD dwWidth;               // Fields specify the width of the AVIfile in pixels.
+    DWORD dwHeight;              // Fields specify the height of the AVIfile in pixels.
+    DWORD dwReserved[4];         // 0, 0, 0, 0
 };
+
+struct AviStreamHeader
+{
+    uint32_t fccType;              // 'vids', 'auds', 'txts'...
+    uint32_t fccHandler;           // "cvid", "DIB "
+    DWORD dwFlags;               // 0
+    DWORD dwPriority;            // 0
+    DWORD dwInitialFrames;       // 0
+    DWORD dwScale;               // 1
+    DWORD dwRate;                // Fps (dwRate - frame rate for video streams)
+    DWORD dwStart;               // 0
+    DWORD dwLength;              // Frames number (playing time of AVI file as defined by scale and rate)
+    DWORD dwSuggestedBufferSize; // For reading the stream
+    DWORD dwQuality;             // -1 (encoding quality. If set to -1, drivers use the default quality value)
+    DWORD dwSampleSize;          // 0 means that each frame is in its own chunk
+    struct {
+        short int left;
+        short int top;
+        short int right;
+        short int bottom;
+    } rcFrame;                // If stream has a different size than dwWidth*dwHeight(unused)
+};
+
+struct BitmapInfoHeader
+{
+    DWORD biSize;                // Write header size of BITMAPINFO header structure
+    LONG  biWidth;               // width in pixels
+    LONG  biHeight;              // heigth in pixels
+    WORD  biPlanes;              // Number of color planes in which the data is stored
+    WORD  biBitCount;            // Number of bits per pixel
+    DWORD biCompression;         // Type of compression used (uncompressed: NO_COMPRESSION=0)
+    DWORD biSizeImage;           // Image Buffer. Quicktime needs 3 bytes also for 8-bit png 
+                                 //   (biCompression==NO_COMPRESSION)?0:xDim*yDim*bytesPerPixel;
+    LONG  biXPelsPerMeter;       // Horizontal resolution in pixels per meter
+    LONG  biYPelsPerMeter;       // Vertical resolution in pixels per meter
+    DWORD biClrUsed;             // 256 (color table size; for 8-bit only)
+    DWORD biClrImportant;        // Specifies that the first x colors of the color table. Are important to the DIB.
+};
+
+struct RiffChunk
+{
+    uint32_t m_four_cc;
+    uint32_t m_size;
+};
+
+struct RiffList
+{
+    uint32_t m_riff_or_list_cc;
+    uint32_t m_size;
+    uint32_t m_list_type_cc;
+};
+
+#pragma pack(pop)
+
+void printFourCc(const uint32_t& four_cc)
+{
+    char result[5];
+    uint32_t* p_4cc = (uint32_t*)result;
+
+    *p_4cc = four_cc;
+
+    result[4] = 0;
+
+    std::cout << result << std::endl;
+}
+
+std::istream& operator >> (std::istream& is, AviMainHeader& avih) 
+{
+    is.read((char*)(&avih), sizeof(AviMainHeader));
+}
+
+std::istream& operator >> (std::istream& is, AviStreamHeader& strh) 
+{
+    is.read((char*)(&strh), sizeof(AviStreamHeader));
+}
+
+std::istream& operator >> (std::istream& is, BitmapInfoHeader& bmph) 
+{
+    is.read((char*)(&bmph), sizeof(BitmapInfoHeader));
+}
+
+std::istream& operator >> (std::istream& is, RiffList& riff_list) 
+{
+    is.read((char*)(&riff_list), sizeof(riff_list));
+}
+
+std::istream& operator >> (std::istream& is, RiffChunk& riff_chunk) 
+{
+    is.read((char*)(&riff_chunk), sizeof(riff_chunk));
+}
 
 class MJpegReaderImpl : public MJpegReader
 {
@@ -34,23 +141,97 @@ public:
     {
         std::fstream fs;
         fs.open (filename.c_str(), std::fstream::in | std::fstream::binary);
-        int riff = 0;
-        int avi = 0;
-        char buf[32];
 
-        //fs.read((char*)&riff, 4);
-        //fs.read((char*)&avi, 4);
-        fs.read(buf, 32);
+        RiffList riff_list;
 
-        std::cout << "is opened " << fs.is_open() << std::endl;
-        //std::cout << "riff " << riff << " " << fourCC('R', 'I', 'F', 'F') << std::endl;
-        //std::cout << "avi " << avi << " " << fourCC('A', 'V', 'I', ' ') << std::endl;
-        for(int i = 0; i < 8; ++i)
+        fs >> riff_list;
+
+        std::cout << (RIFF_CC == fourCC_str("RIFF")) << std::endl;;
+
+        if(riff_list.m_riff_or_list_cc == RIFF_CC)
         {
-            std::cout << buf[i];
-        }
+            if(riff_list.m_list_type_cc == AVI_CC)
+            {
+                std::cout << "AVI" << std::endl;
+                RiffList list;
 
-        std::cout << std::endl;
+                fs >> list;
+
+                if(list.m_riff_or_list_cc == LIST_CC)
+                {
+                    std::cout << "LIST" << std::endl;
+                    std::cout << list.m_size << std::endl;
+
+                    printFourCc(list.m_list_type_cc);
+
+                    RiffChunk hdrl;
+
+                    fs >> hdrl;
+
+                    printFourCc(hdrl.m_four_cc);
+
+                    AviMainHeader avi_head;
+                    fs >> avi_head;
+
+                    fs >> list;
+
+                    if(list.m_riff_or_list_cc == LIST_CC)
+                    {
+                        printFourCc(list.m_list_type_cc);
+                    }
+
+                    fs >> hdrl;
+                    printFourCc(hdrl.m_four_cc);
+
+                    AviStreamHeader strh;
+                    fs >> strh;
+
+                    printFourCc(strh.fccType);
+                    printFourCc(strh.fccHandler);
+
+                    fs >> hdrl;
+                    printFourCc(hdrl.m_four_cc);
+
+                    BitmapInfoHeader bmp_info_header;
+                    fs >> bmp_info_header;
+
+                    std::cout << bmp_info_header.biBitCount << std::endl;
+
+                    printFourCc(bmp_info_header.biCompression);
+
+                    fs >> list;
+
+                    if(list.m_riff_or_list_cc == LIST_CC)
+                    {
+                        std::cout << "LIST" << std::endl;
+                        std::cout << list.m_size << std::endl;
+
+                        printFourCc(list.m_list_type_cc);
+
+                        fs >> hdrl;
+                        printFourCc(hdrl.m_four_cc);
+
+                        std::cout << hdrl.m_size << std::endl;
+
+                        fs.seekg(hdrl.m_size, fs.cur);
+
+                        fs >> hdrl;
+                        printFourCc(hdrl.m_four_cc);
+
+                        fs.seekg(hdrl.m_size, fs.cur);
+
+                        fs >> list;
+                        if(list.m_riff_or_list_cc == LIST_CC)
+                        {
+                            std::cout << "LIST" << std::endl;
+                            std::cout << list.m_size << std::endl;
+
+                            printFourCc(list.m_list_type_cc);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     ~MJpegReaderImpl()
